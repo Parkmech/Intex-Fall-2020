a8ada1fc900dfd22b1c91c38b4e6c1feb5ae33ba
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2020-12-05 19:31:41 -0700

    Changed models to get database working fully

--- applicant/models.py
+++ applicant/models.py
@@ -7,10 +7,10 @@ from django.db.models.fields.related import ForeignKey
 # Create your models here.
 class Applicant(models.Model):
     email = models.EmailField()
-    username = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
     #resume = models.FileField(upload_to='files')
     #photo = models.ImageField(upload_to = 'photos')
 
@@ -18,10 +18,10 @@ class Applicant(models.Model):
         return self.first_name + ' ' + self.last_name
 
 class Skill(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return str(self.name).title()
 
 class ApplicantSkill(models.Model):
     applicant = models.ForeignKey(Applicant, on_delete=CASCADE) 
@@ -30,12 +30,12 @@ class ApplicantSkill(models.Model):
         validators=[MaxValueValidator(4), MinValueValidator(0)])
 
     def __str__(self):
-        return str(self.applicant) + ' ' + str(self.skill)
+        return str(self.applicant) + ' -- ' + str(self.skill).title() + ' -- Level: ' + str(self.skill_level)
 
 class Organization(models.Model):
     company_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    password = models.CharField(max_length=25)
+    password = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     size = models.CharField(max_length=25)
     sector = models.CharField(max_length=2)
@@ -43,22 +43,23 @@ class Organization(models.Model):
 
     def __str__(self):
         return self.company_name
+
+class Contract(models.Model) :
+    type = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return str(self.type)
     
 class Listing(models.Model) :
     status = models.CharField(max_length=20)
-    job_title = models.CharField(max_length=50)
-    description = models.CharField(max_length=1000)
+    job_title = models.CharField(max_length=1000)
+    city = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=12000)
     organization = models.ForeignKey(Organization, on_delete=CASCADE)
+    contract = models.ForeignKey(Contract, on_delete=CASCADE, null=True)
 
     def __str__(self):
         return self.job_title
-    
-class Contract(models.Model) :
-    listing = ForeignKey(Listing, on_delete=CASCADE)
-    type = models.CharField(max_length=2)
-
-    def __str__(self):
-        return str(self.listing) + ' ' + str(self.type)
 
 class SavedListing(models.Model):
     applicant = ForeignKey(Applicant, on_delete=CASCADE)
@@ -81,7 +82,7 @@ class Offer(models.Model):
     application = models.ForeignKey(Application, on_delete=CASCADE)
 
     def __str__(self):
-        return self.text
+        return str(self.application)
 
 class PreferredSkill(models.Model):
     skill = ForeignKey(Skill, on_delete=CASCADE)
@@ -90,4 +91,4 @@ class PreferredSkill(models.Model):
         validators=[MaxValueValidator(4),MinValueValidator(0)])
 
     def __str__(self):
-        return str(self.listing) + ', ' + str(self.skill)  + ' ' + str(self.skill_level) 
+        return str(self.listing) + ' -- ' + str(self.skill).title()  + ' -- Level: ' + str(self.skill_level) 
