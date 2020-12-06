6ade246264d2b90be3a233ba01ea36e177c65d39
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2020-12-05 22:15:27 -0700

    Got the create score function working at a base level.
    Co-authored-by: scottsyoung97 <scottsyoung97@users.noreply.github.com>

--- applicant/urls.py
+++ applicant/urls.py
@@ -1,10 +1,11 @@
 from os import name
 from django.urls import path
-from .views import applicantPageView, resumePageView, skillsPageView, profilePageView
+from .views import applicantPageView, createScore, resumePageView, skillsPageView, profilePageView, createScore
 
 urlpatterns = [
     path('resume/', resumePageView, name='resume'),
     path('skills/', skillsPageView, name='skills'),
     path('profile/', profilePageView, name='profile'),
+    path('test/', createScore, name='test'),          #delete this later
     path('', applicantPageView, name='applicant'),
 ]
--- applicant/views.py
+++ applicant/views.py
@@ -1,11 +1,24 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-#from .models import Applicant
+from .models import Applicant, ApplicantSkill, PreferredSkill, Listing, Skill
 
 # Create your views here.
 def applicantPageView(request):
-    #a = App
-    return render(request, "applicant/landing.html")
+    l = Listing.objects.all()
+    listings = []
+    for i in l:
+        listings.append(i)
+    listings = listings[0:20]
+    skills = PreferredSkill.objects.all()
+
+        
+    
+    context = {
+        'listings' : listings,
+        'skills' : skills
+    }
+    print('test')
+    return render(request, "applicant/landing.html", context)
 
 def skillsPageView(request):
     return render(request, "applicant/skills.html")
@@ -15,4 +28,30 @@ def resumePageView(request):
 
 def profilePageView(request):
     return render(request, "applicant/profile.html")
+
+def createScore(request): #Add the listingId as a parameter
+
+    ids = []
+    preferred_skills = PreferredSkill.objects.filter(listing_id = 44) #this will be the dynamically added listing ID 
+    for skill in preferred_skills:
+       ids.append(skill.skill_id)
+    
+    applicant_groups = []
+    for id in ids:
+        applicant_groups.append(ApplicantSkill.objects.filter(skill_id = id))
+
+    output = ''
+    applicants = {}
+
+    for applicant_group in applicant_groups:
+        for applicant_skill in applicant_group: 
+            if applicant_skill.applicant.id not in applicants:
+                applicants[applicant_skill.applicant.id] = {}
+                applicants[applicant_skill.applicant.id][applicant_skill.skill.id] = applicant_skill.skill_level
+            else:
+                applicants[applicant_skill.applicant.id][applicant_skill.skill.id] = applicant_skill.skill_level
+
+    print(applicants)
+    return HttpResponse(applicants.values())
+    
 
